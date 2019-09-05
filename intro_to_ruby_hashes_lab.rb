397cbf5208905c9634431a4009b346f857e4d9e1
Lialah Barrer <lrbarrer@gmail.com>
2019-09-05 23:12:08 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,7 +24,7 @@ def monopoly_with_third_tier
     	disney = {
 	  :railroads => {:pieces => 4, 
 	    :rent_in_dollars =>{
-	      :one_piece_owned => "25", 
+	      :one_piece_owned => 25, 
 	      :two => "", 
 	      :three => "", 
 	      :four => ""
