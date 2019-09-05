f40334be3d299669795ee07027a08ff9218e811e
Lialah Barrer <lrbarrer@gmail.com>
2019-09-05 23:04:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,8 +22,10 @@ def monopoly_with_third_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
     	disney = {
-	  :railroads => {:pieces => 4, :rent_in_dollars =>{:one => "", :two => "", :three => "", :four => ""}},
-	  :names =>{}
+	  :railroads => {:pieces => 4, 
+	    :rent_in_dollars =>{:one => "", :two => "", :three => "", :four => ""}
+	    :names =>{}
+	  },
   }
   
 end
